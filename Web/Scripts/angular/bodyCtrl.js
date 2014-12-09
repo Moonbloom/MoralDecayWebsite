5921e42cd22bb23d9b4c4a840f032c84b3b342a7
Moonbloom <kasperliholm@hotmail.com>
2014-12-09 19:59:04 +0100

    quick update

--- Web/Scripts/angular/bodyCtrl.js
+++ Web/Scripts/angular/bodyCtrl.js
@@ -1,11 +1,5 @@
 ﻿app = angular.module('mdWebApp', ['ngRoute']);
 
-var homeUrl = '/';
-var forumUrl = '#/forum';
-var recruitmentUrl = '/recruitment';
-var galleryUrl = '/gallery';
-var aboutUsUrl = '/about-us';
-
 app.config(["$routeProvider", function($routeProvider) {
     $routeProvider
         .when('/', {
@@ -23,23 +17,23 @@ app.controller('bodyCtrl', ['$scope', function($scope) {
     $scope.leftNavMenuItems = [
         {
             text: "HOME",
-            href: "/#/"
+            href: "#/"
         },
         {
             text: "FORUM",
-            href: "/#/forum"
+            href: "#/forum"
         },
         {
             text: "RECRUITMENT",
-            href: "/#/recruitment"
+            href: "#/recruitment"
         },
         {
             text: "GALLERY",
-            href: "/#/gallery"
+            href: "#/gallery"
         },
         {
             text: "ABOUT US",
-            href: ""
+            href: "#/about-us"
         }
     ];
 }]);
